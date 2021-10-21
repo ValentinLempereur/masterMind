98762bbfabfcc40893cfec1107bf9a3415223258
Lempereur Valentin <160646@CCM-Asty-Moulin.local>
2021-10-21 16:01:44 +0200

    stokage de la proposition de l'utilisateur dans un tableau

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -21,14 +21,33 @@ namespace MasterMind
         static void Main(string[] args)
         {
             int[] table;
+            int[] compa;
             Color(out table);
             Afficher(table);
+            Console.WriteLine();
+            Console.WriteLine("entrer une combinaison");
+            string combiUti = Console.ReadLine();
+            Comparaison(combiUti, out compa);
+            Afficher(compa);
             Console.ReadLine();
+
+
+
         }
         static void Afficher(int[] table) {
             for (int i = 0; i < 4; i++) {
                 Console.Write(table[i]);       
             }
+        }
+
+        static void Comparaison(string combiUti, out int[] compa) 
+        {
+            compa = new int[4];
+            for (int i = 0; i <= 3; i++) 
+            {
+                compa[i] = combiUti[i];
+            }
+            //Afficher(compa);
         }
     }
 }
